66a978b876d0376abcee32165ad09ac6415370ea
Gabriel Salinas <g.szada@gmail.com>
2022-03-15 10:45:05 +0100

    Added useful comments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import { useState, useRef, useEffect } from 'react'
 
+/** ABOUT useRef
+ * 
+ * - preserves value
+ * - does NOT trigger re-render as useState does
+ * - targets DOM node/elements
+ * 
+ * It can be used to count like useState does and also to
+ * extract data from the elements without without re-rendering the component
+ * everytime. 
+ * 
+ */
+
 export default function App() {
   const count = useRef(0)
   const rendersCount = useRef(1)
   const [forcedRenders, addForcedRender] = useState(0)
   const inputContainer = useRef(null)
 
+  /** FORM SUBMIT HANDLER
+   * 
+   * - event.preventDefault() prevents the page to be reload.
+   * - inputContainer.current returns the input element.
+   */
   function handleSubmit(event) {
     event.preventDefault()
     let inputValue = inputContainer.current.value
     console.log(inputValue)
     inputContainer.current.value = null
   }
+
 
   function handleClick() {
     count.current++
